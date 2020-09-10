4e7db7dc44b624f11a957dda54a29001297390d7
Matthias Kay <matthias.kay@it-consulting-rosengarten.de>
2020-09-10 11:13:06 +0200

    Add dependant objects to output variables

--- src/main/java/org/jqassistant/contrib/plugin/hcl/model/TerraformOutputVariable.java
+++ src/main/java/org/jqassistant/contrib/plugin/hcl/model/TerraformOutputVariable.java
@@ -1,32 +1,38 @@
 package org.jqassistant.contrib.plugin.hcl.model;
 
+import java.util.List;
+
 import com.buschmais.xo.neo4j.api.annotation.Label;
+import com.buschmais.xo.neo4j.api.annotation.Relation;
 
 /**
  * Marks an output variable in a terraform file.
- * 
+ *
  * @author Matthias Kay
  * @since 1.0
  */
 @Label("OutputVariable")
 public interface TerraformOutputVariable extends TerraformBlock {
-  String getName();
-
-  void setName(String name);
-
-  String getType();
+  @Relation("DEPENDS_ON")
+  List<TerraformBlock> getDependantObjects();
 
   String getDescription();
 
-  String getValue();
+  String getName();
 
   String getSensitive();
 
-  void setType(String type);
+  String getType();
+
+  String getValue();
 
   void setDescription(String description);
 
-  void setValue(String value);
+  void setName(String name);
 
   void setSensitive(String sesitive);
+
+  void setType(String type);
+
+  void setValue(String value);
 }
